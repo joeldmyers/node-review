a61c61e67602254a1d1563f2e15c743d52b5ad51
Joel Myers <joeldmyers@gmail.com>
2018-03-27 00:06:49 -0400

    first pass at conditional based on arguments

--- notes/app.js
+++ notes/app.js
@@ -2,3 +2,18 @@ console.log('starting app');
 
 const fs = require('fs');
 const notes = require('./notes');
+const _ = require('lodash');
+
+let command = process.argv[2];
+
+console.log(`Command: ${command}`);
+
+if (command === 'add') {
+  // add new note
+  console.log('Adding new note');
+} else if (command === 'list') {
+  // list notes
+  console.log('Listing all notes');
+} else {
+  console.log('command not recognized');
+}
