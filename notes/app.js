505ce50fa92a3521d23ba4bc068b6b75027651df
Joel Myers <joeldmyers@gmail.com>
2018-03-27 00:01:02 -0400

    cleanup

--- notes/app.js
+++ notes/app.js
@@ -1,13 +1,4 @@
 console.log('starting app');
 
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes');
-
-
-
-// let user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
-
-console.log(notes.addNumbers(5,4));
